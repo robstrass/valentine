b558c70131a2a611726ffee7cd40512eb5e0b6d4
Rob Strasser <robstrasserdev@gmail.com>
2024-02-04 17:46:56 -0800

    working product

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Mega sure?",
+  "Mega ultra sure?",
+  "Mega ultra premium supreme sure?",
+  "Wait pls no...",
+  "Y u do this to me?",
+  "Pls, am gonna be sad",
+  "Gonna cry :(",
+];
+
+export default function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [isYes, setIsYes] = useState(false);
+  const yesBtnSize = noCount * 20 + 16;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="valentineContainer">
+      {isYes ? (
+        <>
+          <div className="imageContainer">
+            <img
+              alt="bears hugging with hearts"
+              className="yesImg"
+              src="https://tenor.com/view/milk-and-mocha-gif-21799600.gif"
+            />
+          </div>
+          <div>
+            YAY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="imageContainer">
+            <img
+              alt="bear bouncing with flowers in mouth"
+              className="noImg"
+              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            />
+          </div>
+          <div>Will you be my Valentine?</div>
+          <div className="buttonContainer">
+            <button
+              className="yesButton"
+              onClick={() => setIsYes(true)}
+              style={{ fontSize: yesBtnSize }}
+            >
+              Yes
+            </button>
+            <button
+              className="noButton"
+              onClick={() => handleClick({ noCount, setNoCount })}
+            >
+              {getNoText({ noCount })}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
-export default App;
+function handleClick({ noCount, setNoCount }) {
+  setNoCount(noCount + 1);
+}
+
+function getNoText({ noCount }) {
+  return phrases[Math.min(noCount, phrases.length - 1)];
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div>
+      Inspired by
+      <span>
+        {" "}
+        <a href="https://www.tiktok.com/@mewtru">mewtru</a>
+      </span>
+    </div>
+  );
+}
